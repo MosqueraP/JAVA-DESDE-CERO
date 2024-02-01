a447e681ed62fca17e2b8a95530f90ff137d4143
Plinio <mcplinio.15@gmail.com>
2024-02-01 18:00:11 -0500

    Sumatorio de lista

---
+++ src/com/Array04/ForEachTarea16.java
@@ -0,0 +1,28 @@
+package src.com.Array04;
+
+public class ForEachTarea16 {
+    public static void main(String[] args) {
+        /* #1
+        Crea un Array con varios números enteros. Calcula la suma de
+        todos los números dentro del Array.
+         */
+        int [] listNumeros = {1, 2, 3, 4};
+
+        int sumatorio = 0;
+
+        for (int suma : listNumeros){
+            sumatorio += suma;
+        }
+        System.out.println(sumatorio);
+
+
+        /*#2
+        Crea un Array con varias palabras. Imprime todas las palabras
+        que tengan una longitud mayor que 5 caracteres.
+         */
+        /* #3
+        Crea un Array con varios números enteros. Encuentra el promedio
+         de todos los elementos.
+         */
+    }
+}
