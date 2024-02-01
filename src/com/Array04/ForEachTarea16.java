7f6f612fd60d79857271e927506d8ad57f8b1cf8
Plinio <mcplinio.15@gmail.com>
2024-02-01 18:09:50 -0500

    Tarea for if: imprimir las pabras que tengas mas de cinco caracteres

--- src/com/Array04/ForEachTarea16.java
+++ src/com/Array04/ForEachTarea16.java
@@ -15,11 +15,19 @@ public class ForEachTarea16 {
         }
         System.out.println(sumatorio);
 
-
         /*#2
         Crea un Array con varias palabras. Imprime todas las palabras
         que tengan una longitud mayor que 5 caracteres.
          */
+
+        String [] palabras = {"Pc", "Lapto", "Impresora", "Monitor"};
+        for (int i = 0; i < palabras.length; i++) {
+            if (palabras[i].length() > 5){
+                System.out.println(palabras[i]);
+            }
+        }
+
+
         /* #3
         Crea un Array con varios números enteros. Encuentra el promedio
          de todos los elementos.
