3edd5e51434ca25d69a353751ca09de836126dc3
Plinio <mcplinio.15@gmail.com>
2024-02-01 20:53:14 -0500

    encapsulacion, principios

---
+++ src/com/sem_04/Ejemplo.java
@@ -0,0 +1,8 @@
+package src.com.sem_04;
+
+public class Ejemplo {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setNombre("Howard");
+    }
+}
