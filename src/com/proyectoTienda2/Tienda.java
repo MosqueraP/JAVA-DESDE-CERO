35220fef301dbcaa22fb84881e47e7cc31e6348e
Plinio <mcplinio.15@gmail.com>
2024-02-02 12:19:42 -0500

    crear proyecto tienda

---
+++ src/com/proyectoTienda2/Tienda.java
@@ -0,0 +1,14 @@
+package src.com.proyectoTienda2;
+
+public class Tienda {
+    private String nombreTienda;
+    private String ubicacion;
+
+
+    public Tienda(String nombreTienda, String ubicacion) {
+        // constructor de la tienda
+        this.nombreTienda = nombreTienda;
+        this.ubicacion = ubicacion;
+    }
+
+}
