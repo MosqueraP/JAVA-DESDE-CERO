32cfaacd5ee3ce00a189983407b20e8f5ea7a22b
Plinio <mcplinio.15@gmail.com>
2024-02-01 21:13:49 -0500

    getter y setter modificadores

---
+++ src/com/sem_04/CuentaDeBanco.java
@@ -0,0 +1,30 @@
+package src.com.sem_04;
+
+public class CuentaDeBanco {
+    private String titular;
+    public double balance;
+
+    // getter -> obtener
+    // metodo
+    public double getBalance() {
+        return this.balance;
+    }
+
+    // getter -> obtener
+    public String getTitular() {
+        return this.titular;
+    }
+
+    // setter -> cambiar-modificar el balance
+    // metodo modificdor
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    // setter -> cambiar-modificar el titular
+    // metodo modificdor
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+}
