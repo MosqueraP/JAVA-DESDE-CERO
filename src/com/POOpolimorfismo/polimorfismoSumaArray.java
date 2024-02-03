b679d54ec20ab575a8109d62450a65492c220e41
Plinio <mcplinio.15@gmail.com>
2024-02-02 20:06:28 -0500

    Poo-polimorfismo- ejercicio suma de arrays

---
+++ src/com/POOpolimorfismo/polimorfismoSumaArray.java
@@ -0,0 +1,48 @@
+package src.com.POOpolimorfismo;
+
+import java.util.Arrays;
+
+public class polimorfismoSumaArray {
+    /*
+     1mera parte:
+     Crea un método que tome un array de enteros como argumento y devuelva la suma
+     de todos los elementos del array.
+     2da parte:
+     Luego, crea otro método con el mismo nombre que tome dos arrays de enteros
+     como argumentos y devuelva un nuevo array que
+     contenga la suma de los elementos correspondientes de cada array
+     (por ejemplo, si el primer array es {1, 2, 3} y el segundo es {4, 5, 6},
+     el nuevo array sería {5, 7, 9}).
+     */
+
+    // suma de datos del array
+    static int arraySuma(int[] array){
+        int sumaTotal = 0;
+        for (int i = 0; i < array.length; i++){
+            sumaTotal += array[i];
+
+        }
+        return sumaTotal;
+    }
+
+    // suma de dos arrays
+    static int[] arraySuma(int[] array, int[] array2){
+        int[] arrayFinal = new int[array.length];
+
+        for (int i = 0; i < array.length; i++){
+            arrayFinal[i] = array[i] + array2[i];
+
+        }
+        return arrayFinal;
+    }
+
+    public static void main(String[] args) {
+        // metodo principal
+
+        int array[] = {1, 2, 3};
+        int array2[] = {0, 4, 3};
+
+        System.out.println(arraySuma(array));
+        System.out.println(Arrays.toString(arraySuma(array, array2)));
+    }
+}
