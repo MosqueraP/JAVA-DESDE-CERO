d1a9432a5be40cdfd2d23dd9fd4d8751947c8b82
Plinio <mcplinio.15@gmail.com>
2024-02-03 00:33:44 -0500

    Poo-polimorfismo - Saludos de personas

---
+++ src/com/POOpolimorfismo/Saludo/Estudiante.java
@@ -0,0 +1,8 @@
+package src.com.POOpolimorfismo.Saludo;
+
+public class Estudiante extends Persona{
+    @Override
+    public void presnetarse() {
+        System.out.println("Hola, soy un estudiante ");
+    }
+}
---
+++ src/com/POOpolimorfismo/Saludo/Persona.java
@@ -0,0 +1,18 @@
+package src.com.POOpolimorfismo.Saludo;
+
+public class Persona {
+
+    /*
+    #1. Crea una clase Persona con un método presentarse() que imprima
+    "Hola, soy una persona".
+    #2. Luego, crea una clase Estudiante que herede de Persona y
+    sobrescriba el método presentarse() para imprimir
+    "Hola, soy un estudiante".
+    #3. Crea un objeto de la clase Estudiante y llama al método
+    presentarse() para verificar que imprime "Hola, soy un estudiante".
+     */
+
+    public void presnetarse() {
+        System.out.println("Hola, soy una persona ");
+    }
+}
