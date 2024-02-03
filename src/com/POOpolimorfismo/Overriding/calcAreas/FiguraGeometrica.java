0a71df9db967ef0b3f792c4fb99fc1a48d37a222
Plinio <mcplinio.15@gmail.com>
2024-02-03 00:21:41 -0500

    Poo-polimorfismo - calculo de areas

---
+++ src/com/POOpolimorfismo/Overriding/calcAreas/FiguraGeometrica.java
@@ -0,0 +1,20 @@
+package src.com.POOpolimorfismo.Overriding.calcAreas;
+
+public class FiguraGeometrica {
+
+    /*
+    #1. Crea una clase FiguraGeometrica con un método calcularArea() que tome
+    los parámetros base y altura, y devuelva 0.
+
+    #2. Luego, crea una clase Rectangulo que herede de FiguraGeometrica y
+    sobrescriba el método calcularArea() para calcular el área del
+    rectángulo (multiplicando su base por su altura).
+
+    #3. Crea un objeto de la clase Rectangulo con una base de 5 y una
+    altura de 10, y llama al método calcularArea() para verificar que
+    devuelve 50.
+     */
+    public double calcularArea(double base, double altura) {
+        return 0;
+    }
+}
