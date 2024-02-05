3d4049251df9047161cc93335f870dab1c566f27
Plinio <mcplinio.15@gmail.com>
2024-02-04 21:56:49 -0500

    Poo-interfaces,implementacion base de datos PSGT

---
+++ src/com/abstraccion/interfac/PostgresSQL.java
@@ -0,0 +1,13 @@
+package src.com.abstraccion.interfac;
+
+public class PostgresSQL implements BaseDatos {
+    @Override
+    public void conectar() {
+
+    }
+
+    @Override
+    public void desconectar() {
+
+    }
+}
