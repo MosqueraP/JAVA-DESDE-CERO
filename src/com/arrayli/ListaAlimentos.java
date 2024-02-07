2bf3143af31b358c25e1077834db80e771d4e435
Plinio <mcplinio.15@gmail.com>
2024-02-06 20:02:07 -0500

    ArrayList en java

---
+++ src/com/arrayli/ListaAlimentos.java
@@ -0,0 +1,22 @@
+package src.com.arrayli;
+
+import java.util.ArrayList;
+
+public class ListaAlimentos {
+    public static void main(String[] args) {
+        // definicion de Arraylist
+        //ArrayList<String> lista = new ArrayList<>(); // almacena solo strin
+        ArrayList lista = new ArrayList<>(); // almacena de todo
+        lista.add("Manzana"); // Añadir alimento a la lista
+        lista.add("Mango"); //
+        lista.add("Fresa"); //
+        //lista.add(2);
+        System.out.println(lista.size()); // tamaño de la lista
+        System.out.println(lista);
+        System.out.println(lista.indexOf("Fresa")); // indice
+        System.out.println(lista.remove("Manzana")); // elimina
+        System.out.println(lista);
+        System.out.println(lista.size()); // tamaño de la lista
+
+    }
+}
