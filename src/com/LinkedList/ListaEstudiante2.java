1cff1ed7a44b5f2a08094c4ed0889e959d473411
Plinio <mcplinio.15@gmail.com>
2024-02-06 21:45:06 -0500

    LinkedList hashCode ver direccion de memoria de objetos

---
+++ src/com/LinkedList/ListaEstudiante2.java
@@ -0,0 +1,14 @@
+package src.com.LinkedList;
+
+public class ListaEstudiante2 {
+    public static void main(String[] args) {
+        String palabra = "palabra";
+        String palabra1 = "pala";
+        String palabra2 = "p";
+        int[] list ={1,2,3,4,5 };
+
+        System.out.println(palabra2.hashCode()); // direccion de memoria
+        System.out.println(list); // direccion de memoria
+
+    }
+}
