1f9491261bf230a4b1f7af5a9df3321ddf3c3b50
Plinio <mcplinio.15@gmail.com>
2024-02-08 15:58:57 -0500

    Proyecto, Cosntructor métodos y getters y setters

--- src/com/ProyectoListaTareas/Tarea.java
+++ src/com/ProyectoListaTareas/Tarea.java
@@ -1,4 +1,30 @@
 package src.com.ProyectoListaTareas;
 
 public class Tarea {
+    // atributos de clases
+    private String nombre;
+    private boolean completada;
+
+    // constructor
+    public Tarea(String nombre, boolean completada) {
+        this.nombre = nombre;
+        this.completada = false; // al crear la tareas esta en false no completada
+    }
+
+    // getters / setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
 }
