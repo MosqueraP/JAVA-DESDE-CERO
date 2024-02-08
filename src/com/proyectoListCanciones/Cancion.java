aa0f7f7ab3e82cb74a33f6f07279050159fd1669
Plinio <mcplinio.15@gmail.com>
2024-02-08 18:23:22 -0500

    Proyecto, Lista de canciones usando playlist

---
+++ src/com/proyectoListCanciones/Cancion.java
@@ -0,0 +1,11 @@
+package src.com.proyectoListCanciones;
+
+public class Cancion {
+    /*
+    Crea un programa que permita tener canciones en una playlist.
+    El usuario debe tener la opción de añadir canciones a la playlist,
+    eliminarlas, verlas, y buscar por artista.
+
+*usando LinkedList
+     */
+}
---
+++ src/com/proyectoListCanciones/Playlist.java
@@ -0,0 +1,4 @@
+package src.com.proyectoListCanciones;
+
+public class Playlist {
+}
