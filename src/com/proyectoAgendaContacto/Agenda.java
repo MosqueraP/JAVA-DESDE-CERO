fcb211ebb5de75e2e2e1a381486e98329d67c6ab
Plinio <mcplinio.15@gmail.com>
2024-02-08 21:41:05 -0500

    proyecto agregar métodos en la agenda de contactos

--- src/com/proyectoAgendaContacto/Agenda.java
+++ src/com/proyectoAgendaContacto/Agenda.java
@@ -1,4 +1,21 @@
 package src.com.proyectoAgendaContacto;
 
+import java.util.HashMap;
+
 public class Agenda {
+    private static HashMap<String, Contacto> contactos = new HashMap<>();
+
+    public static void agregarContacto(Contacto contacto) {
+        contactos.put(contacto.getNombre(), contacto);
+        System.out.println("Contacto agregado");
+    }
+
+    public static void eliminarContacto(String nombre) {
+        contactos.remove(nombre);
+
+    }
+
+    public static Contacto bucarContacto(String nombre) {
+        return contactos.get(nombre);
+    }
 }
