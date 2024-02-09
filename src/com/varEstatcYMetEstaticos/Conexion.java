33c5057c1b993f09e6b99ae70d22063dfcea6cc0
Plinio <mcplinio.15@gmail.com>
2024-02-08 21:17:33 -0500

    variables estatic

---
+++ src/com/varEstatcYMetEstaticos/Conexion.java
@@ -0,0 +1,23 @@
+package src.com.varEstatcYMetEstaticos;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    public static String baseDatos;
+
+    // se ejecuta antes de cualquier otra cosa
+    // muy util en las bases de datos
+    static {
+        try {
+            // establecer la conexion bd
+            baseDatos = DriverManager.getConnection("howard.com", "usuario", "contraseña")
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
