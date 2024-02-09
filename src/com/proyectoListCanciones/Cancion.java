7de8de60624a514be484b9dc655deef57df63480
Plinio <mcplinio.15@gmail.com>
2024-02-08 19:32:45 -0500

    Proyecto, Lista de canciones terminado

--- src/com/proyectoListCanciones/Cancion.java
+++ src/com/proyectoListCanciones/Cancion.java
@@ -1,11 +1,41 @@
 package src.com.proyectoListCanciones;
 
 public class Cancion {
+    private String nombre;
+    private String artista;
     /*
     Crea un programa que permita tener canciones en una playlist.
     El usuario debe tener la opción de añadir canciones a la playlist,
     eliminarlas, verlas, y buscar por artista.
 
-*usando LinkedList
+    *usando LinkedList
      */
+
+    // método constructor de Caciones
+    public Cancion(String nombre, String artista) {
+        this.nombre = nombre;
+        this.artista = artista;
+    }
+
+    // getters / setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " - " + artista;
+    }
 }
