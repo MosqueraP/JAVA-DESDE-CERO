4b20f88d2a02a66f1fa036a7bcf05a2a9927f673
Plinio <mcplinio.15@gmail.com>
2024-02-09 09:40:23 -0500

    Threds, multi hilos

---
+++ src/com/multitaskingporHilos/Ejemplo.java
@@ -0,0 +1,9 @@
+package src.com.multitaskingporHilos;
+
+import javax.sound.midi.Soundbank;
+
+public class Ejemplo {
+    public static void main(String[] args) {
+        System.out.println(5/0);
+    }
+}
