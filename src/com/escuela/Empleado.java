2bb07b9070f676a9c435c40c064d2cea68a8eac9
Plinio <mcplinio.15@gmail.com>
2024-06-18 18:06:10 -0500

    Modificar clases erradas

--- src/com/escuela/Empleado.java
+++
@@ -1,7 +0,0 @@
-package com.escuela;
-
-public class Empleado {
-    int idEmpleado = 12434342;
-    String nombre = "nombre";
-    double salario = 500.000;
-}
--- src/com/escuela/Persona.java
+++
@@ -1,7 +0,0 @@
-package com.escuela;
-
-public class Persona {
-    String nombre = "Howard";
-    int edad = 20;
-    String direccion = "Medellin";
-}
